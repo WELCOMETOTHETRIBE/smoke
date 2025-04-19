3f4e820a78b658b3b5da4038f45d61d4444df263
WELCOMETOTHETRIBE <patrick@welcometothetribe.com>
2025-04-18 18:20:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,22 @@
 // script.js
-const canvas = document.getElementById('canvas');
-const gl = canvas.getContext('webgl2', { alpha: false });
-if (!gl) alert('WebGL2 not supported');
+fetch('https://cdn.jsdelivr.net/gh/PavelDoGreat/WebGL-Fluid-Simulation/script.js')
+  .then(res => res.text())
+  .then(code => {
+    const script = document.createElement('script');
+    script.innerHTML = code;
+    document.body.appendChild(script);
 
-function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
-resizeCanvas();
-window.addEventListener('resize', resizeCanvas);
-
-const pointers = [{ id: -1, down: false, moved: false, x: 0, y: 0 }];
-canvas.addEventListener('pointerdown', (e) => {
-  pointers[0].down = true;
-  pointers[0].x = e.clientX;
-  pointers[0].y = e.clientY;
-});
-canvas.addEventListener('pointermove', (e) => {
-  if (pointers[0].down) {
-    pointers[0].moved = true;
-    pointers[0].x = e.clientX;
-    pointers[0].y = e.clientY;
-  }
-});
-canvas.addEventListener('pointerup', () => pointers[0].down = false);
-
-function drawSmokeTrail() {
-  const ctx = canvas.getContext('2d');
-  ctx.fillStyle = 'rgba(0,0,0,0.05)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  if (pointers[0].down && pointers[0].moved) {
-    const r = 180 + Math.random() * 60;
-    const g = 180 + Math.random() * 60;
-    const b = 180 + Math.random() * 60;
-    ctx.beginPath();
-    ctx.arc(pointers[0].x, pointers[0].y, 50, 0, Math.PI * 2);
-    ctx.fillStyle = `rgba(${r}, ${g}, ${b}, 0.1)`;
-    ctx.fill();
-  }
-  requestAnimationFrame(drawSmokeTrail);
-}
-drawSmokeTrail();
+    const configScript = document.createElement('script');
+    configScript.innerHTML = `
+      setTimeout(() => {
+        config.SIM_RESOLUTION = 128;
+        config.DYE_RESOLUTION = 512;
+        config.SHADING = true;
+        config.BLOOM = true;
+        config.TRANSPARENT = false;
+        config.BACK_COLOR = { r: 0, g: 0, b: 0 };
+        multipleSplats(24);
+      }, 1000);
+    `;
+    document.body.appendChild(configScript);
+  });
